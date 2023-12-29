d4bc4ff948ff6031359336594638eeb67442ced2
Danny <daniel18700@outlook.com>
2023-12-29 11:48:18 -0800

    added simple and advanced tabs (not able to switch back to simple tab)

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 
+
+// This simply waits until the webpage is fully loaded to start the code
 document.addEventListener("DOMContentLoaded", function () {
+
+
+
+    //page elements for simple and advanced
+
+    const simpleview = document.getElementById('simpleview');
+    const advancedview = document.getElementById('advancedview');
+    const simplebutton = document.getElementById('simple');
+    const advancedbutton = document.getElementById('advanced');
+
+    simplebutton.addEventListener("click",showsimpleview);
+    advancedbutton.addEventListener("click",showadvancedview);
+
+    function showsimpleview(){
+        simpleview.style.display = 'block';
+        advancedview.style.display = 'none';
+    }
+
+    function showadvancedview() {
+        simpleview.style.display = 'none';
+        advancedview.style.display = 'block';
+    }
+
+    showsimpleview();
+    
+
+
     // Accessing elements
+    //document.getElementById allows to grab whatevers inside of an id, "totalamount" would take whatever is inside of that input, such as a number
+    
     const totalAmountInput = document.getElementById("totalamount");
     const monthsInput = document.getElementById("months");
     const submitButton = document.getElementById("submit");
-
+    
     // Adding click event listener
+    //this allows a function to play when the user clicks the submit button
     submitButton.addEventListener("click", calculateBudget);
 
     // Function to calculate the budget
@@ -20,6 +52,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
             // saveToLocalStorage(totalAmount,numberOfMonths);
 
+
+
         } else {
             alert("Please enter valid values for total amount and number of months.");
         }
@@ -33,4 +67,9 @@ document.addEventListener("DOMContentLoaded", function () {
         budget.textContent = "Monthly Budget: " + monthlyBudget.toFixed(2);
 
     }
+
+
+
+    
+    
 });
